7791d12cd5528164e9bfe082efd8791bf0ed415d
Logan Moonie <school@lmoonie.ca>
2024-07-12 16:00:23 -0700

    Defined types to be used for storing and processing CNF expressions.

---
+++ include/cnf.hpp
@@ -0,0 +1,26 @@
+// cnf.hpp
+// Logan Moonie
+// Jul 12, 2024
+
+#include <unordered_set>
+
+// a collection of types used to store CNF problems
+namespace cnf {
+
+    // a boolean variable;
+    using variable = int;
+
+    // a boolean literal; negative values indicate negation
+    using literal = int;
+
+    // a disjunction of literals
+    using disjunctive_clause = std::unordered_set<literal>;
+
+    // a cnf expression
+    using cnf_expr = std::unordered_set<disjunctive_clause>;
+
+    // a map of variables to their proposed value
+    using solution = std::unordered_map<variable, bool>;
+
+}
+
