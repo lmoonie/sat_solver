a03f13d993bba2f791e235402097268b844a691a
Logan Moonie <school@lmoonie.ca>
2024-07-13 16:09:09 -0700

    Changed how cnf_expr is declared. It is now defined in cnf.hpp, but its members are defined in cnf.cpp.

--- include/cnf.hpp
+++ include/cnf.hpp
@@ -3,6 +3,7 @@
 // Jul 12, 2024
 
 #include <map>
+#include <unordered_set>
 
 // a collection of types used to store CNF problems
 namespace cnf {
@@ -27,7 +28,37 @@ namespace cnf {
     using solution = std::map<variable, bool>;
 
     // a complete boolean CNF expression
-    class cnf_expr;
+    class cnf_expr {
+    public:
+        // default constructor
+        cnf_expr() = default;
+        // input stream constructor
+        cnf_expr(std::istream& istr);
+        // copy constructor
+        cnf_expr(const cnf_expr&) = default;
+        // move constructor
+        cnf_expr(cnf_expr&&) = default;
+        // no assignment permitted
+        cnf_expr& operator=(const cnf_expr&) = delete;
+        cnf_expr& operator=(cnf_expr&&) = delete;
+        // add a literal to a clause
+        void add_literal(literal, clause);
+        // remove a literal from a clause
+        void remove_literal(literal, clause);
+        // remove an entire clause
+        void remove_clause(clause);
+        // give the number of active clauses
+        std::size_t num_clauses();
+        // give the number of active variables
+        variable num_variables();
+    private:
+        // tracks which clauses a given literal is in
+        std::unordered_map<literal, cl_set> literals;
+        // tracks which literals are in a given clause
+        std::unordered_map<clause, lit_set> clauses;
+        // used to print cnf_expr
+        friend std::ostream& operator<<(std::ostream&, const cnf_expr&);
+    };
 
 }
 
--- lib/cnf.cpp
+++ lib/cnf.cpp
@@ -13,71 +13,62 @@ namespace cnf {
 
     using std::abs;
 
-    class cnf_expr {
-    public:
-        // default constructor
-        cnf_expr() = default;
-        // istream constructor
-        cnf_expr(std::istream& istr) {
-            ::io::prob::extract_cnf_problem(istr, *this);
-        }
-        // copy constructor
-        cnf_expr(const cnf_expr&) = default;
-        // move constructor
-        cnf_expr(cnf_expr&&) = default;
-        // no assignment permitted
-        cnf_expr& operator=(const cnf_expr&) = delete;
-        cnf_expr& operator=(cnf_expr&&) = delete;
+    // input stream constructor
+    cnf_expr::cnf_expr(std::istream& istr) {
+        ::io::prob::extract_cnf_problem(istr, *this);
+    }
 
-        void add_literal(literal lit, clause cl) {
-            if (!literals.contains(abs(lit))) {
-                literals.insert({abs(lit), cl_set()});
-                literals.insert({-abs(lit), cl_set()});
-            }
-            literals[lit].insert(cl);
-            if (!clauses.contains(cl)) {
-                clauses.insert({cl, lit_set()});
-            }
-            clauses[cl].insert(lit);
+    // add a literal to a clause
+    void cnf_expr::add_literal(literal lit, clause cl) {
+        if (!literals.contains(abs(lit))) {
+            literals.insert({abs(lit), cl_set()});
+            literals.insert({-abs(lit), cl_set()});
         }
-
-        void remove_literal(literal lit, clause cl) {
-            literals[lit].erase(cl);
-            clauses[cl].erase(lit);
+        literals[lit].insert(cl);
+        if (!clauses.contains(cl)) {
+            clauses.insert({cl, lit_set()});
         }
+        clauses[cl].insert(lit);
+    }
 
-        void remove_clause(clause cl) {
-            for (const literal& lit : clauses[cl]) {
-                literals[lit].erase(cl);
-            }
+    // remove a literal from a clause
+    void cnf_expr::remove_literal(literal lit, clause cl) {
+        literals[lit].erase(cl);
+        clauses[cl].erase(lit);
+        if (clauses[cl].empty()) {
             clauses.erase(cl);
         }
+    }
 
-        std::size_t num_clauses() {
-            return clauses.size();
+    // remove an entire clause
+    void cnf_expr::remove_clause(clause cl) {
+        for (const literal& lit : clauses[cl]) {
+            literals[lit].erase(cl);
         }
+        clauses.erase(cl);
+    }
 
-        variable num_variables() {
-            return literals.size()/2;
-        }
+    // give the number of active clauses
+    std::size_t cnf_expr::num_clauses() {
+        return clauses.size();
+    }
 
-    private:
-        // tracks which clauses a given literal is in
-        std::unordered_map<literal, cl_set> literals;
-        // tracks which literals are in a given clause
-        std::unordered_map<clause, lit_set> clauses;
+    // give the number of active variables
+    variable cnf_expr::num_variables() {
+        return literals.size()/2;
+    }
 
-        friend std::ostream& operator<<(std::ostream& ostr, const cnf_expr& expr) {
-            auto clause_iter(expr.clauses.begin());
-            while (clause_iter != expr.clauses.end()) {
-                std::cout << "{ ";
-                for (literal lit : clause_iter->second) {
-                    std::cout << lit << " ";
-                }
-                std::cout << "}" << std::endl;
-                clause_iter++;
+    // used to print cnf_expr
+    std::ostream& operator<<(std::ostream& ostr, const cnf_expr& expr) {
+        auto clause_iter(expr.clauses.begin());
+        while (clause_iter != expr.clauses.end()) {
+            std::cout << "{ ";
+            for (literal lit : clause_iter->second) {
+                std::cout << lit << " ";
             }
-            return ostr;
+            std::cout << "}" << std::endl;
+            clause_iter++;
         }
-    };
+        return ostr;
+    }
 }
