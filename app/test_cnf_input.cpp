6307bce962631f53f7cd284d5162303ad7a11749
Logan Moonie <school@lmoonie.ca>
2024-07-13 16:33:12 -0700

    Enclosed return statement in curly braces.

--- app/test_cnf_input.cpp
+++ app/test_cnf_input.cpp
@@ -9,6 +9,8 @@ int main() {
     try {
         cnf::cnf_expr expr(std::cin);
         std::cout << expr;
-    } catch (...) return 1;
+    } catch (...) {
+        return 1;
+    }
     return 0;
 }
