140a143ec8b09752231dfa93587c155bedd82a2b
Logan Moonie <school@lmoonie.ca>
2024-08-01 11:19:59 -0700

    Removed duplicate function declaration.

--- lib/sol.cpp
+++ lib/sol.cpp
@@ -80,11 +80,6 @@ namespace sol {
         type = ptype;
     }
 
-    // get problem type
-    ProblemType solution::get_type() const {
-        return type;
-    }
-
     // used to print solution
     std::ostream& operator<<(std::ostream& ostr, const solution& sol) {
         // print solution line
